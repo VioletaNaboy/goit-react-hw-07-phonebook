a11ba49645a7443e6fe9b2e0e11ec72ea8d3be7d
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2023-08-29 13:13:24 +0200

    fixed_contactsSlice.js_deleteContact

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -35,7 +35,7 @@ export const contactsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const index = state.items.findIndex(item => item.id === action.payload);
+      const index = state.items.findIndex(item => item.id === action.payload.id);
       state.items.splice(index, 1);
     },
     [deleteContact.rejected]: handleRejected,
